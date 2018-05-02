ba9a24d33e10cfb5dd532381e55d9e79f710c009
Patrick <patricw2@uci.edu>
2018-05-01 18:18:24 -0700

    Deleted extra files

--- SQLDay2/gibberish.rb
+++
@@ -1,7 +0,0 @@
-SELECT SUM(question_likes.user_id) / CAST(COUNT(questions.user_id) AS FLOAT)
-FROM questions
-LEFT OUTER JOIN
-question_likes
-ON
-questions.id = question_likes.questions_id
-GROUP BY questions.id;
